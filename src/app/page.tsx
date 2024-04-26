1fc18661513932962f32a157b3366cc8202c5208
Nishant Yadav <inishantyadav24@gmail.com>
2024-04-27 02:36:23 +0530

    Build footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,15 +7,15 @@ import fileUploadPreview from '../../public/file-upload-preview.jpg'
 
 const HomePage = () => {
   return (
-    <MaxWidthWrapper className="grid grid-rows-3 space-y-1 border border-red-800">
+    <MaxWidthWrapper className="grid grid-rows-3 space-y-1">
       <div className="flex flex-col items-center justify-center space-y-5 text-center">
         <HeaderSection />
       </div>
       <div className="flex flex-col items-center justify-center">
         <MainSection />
       </div>
-      <div className="border border-red-200">
-        <h1>Footer</h1>
+      <div className="flex items-center justify-center">
+        <FooterSection />
       </div>
     </MaxWidthWrapper>
   )
@@ -27,7 +27,7 @@ const HeaderSection = () => {
       <p className="my-5 rounded-full bg-gradient-to-r from-blue-600 to-purple-300 px-7 py-2 font-semibold text-white shadow-md transition-all md:text-lg lg:text-lg">
         Klipp is now public!
       </p>
-      <h1 className="max-w-4xl text-5xl font-bold md:text-6xl lg:text-7xl">
+      <h1 className="max-w-4xl text-5xl font-bold text-slate-600/90 md:text-6xl lg:text-7xl">
         Chat with your <span className="text-blue-600">documents</span> in
         seconds.
       </h1>
@@ -58,6 +58,59 @@ const MainSection = () => {
         className="rounded-lg shadow-2xl ring-1 ring-gray-900/10"
       />
     </div>
+  )
+}
+
+const FooterSection = () => {
+  return (
+    <>
+      <ol className="space-y-4 px-2 md:flex md:space-x-12 md:space-y-0">
+        <li className="md:flex-1">
+          <div className="flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4 md:border-l-0 md:border-t-2 md:pb-0 md:pl-0 md:pt-4">
+            <span className="text-sm font-medium text-blue-600">Step 1</span>
+            <span className="text-xl font-semibold text-slate-700">
+              Sign up for an account
+            </span>
+            <p className="mt-2 text-zinc-700">
+              Either starting out with a free plan or choose our{' '}
+              <span>
+                <Link
+                  href="/pricing"
+                  className="text-blue-600 hover:underline hover:underline-offset-2"
+                >
+                  pro
+                </Link>
+              </span>{' '}
+              plan.
+            </p>
+          </div>
+        </li>
+        <li className="md:flex-1">
+          <div className="flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4 md:border-l-0 md:border-t-2 md:pb-0 md:pl-0 md:pt-4">
+            <span className="text-sm font-medium text-blue-600">Step 2</span>
+            <span className="text-xl font-semibold text-slate-700">
+              Upload your PDF file
+            </span>
+            <span className="mt-2 text-zinc-700">
+              We&apos;ll process your file and make it ready for you to chat
+              with.
+            </span>
+          </div>
+        </li>
+        <li className="md:flex-1">
+          <div className="flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4 md:border-l-0 md:border-t-2 md:pb-0 md:pl-0 md:pt-4">
+            <span className="text-sm font-medium text-blue-600">Step 3</span>
+            <span className="text-xl font-semibold text-slate-700">
+              Start asking questions
+            </span>
+            <span className="mt-2 text-zinc-700">
+              It&apos;s that simple. Try out Klipp today - it really takes less
+              than a minute.
+            </span>
+          </div>
+        </li>
+      </ol>
+    </>
   )
 }
 
