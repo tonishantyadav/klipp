11e2b63ee0a23508235117515b80458838d3056c
Nishant Yadav <inishantyadav24@gmail.com>
2024-04-26 21:14:04 +0530

    Build hero section of a homepage

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import { cn } from '@/lib/utils'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +17,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body
+        className={cn(
+          'absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]',
+          inter.className
+        )}
+      >
+        <div className="absolute bottom-0 left-0 right-0 top-0 bg-[radial-gradient(circle_800px_at_100%_200px,#d5c5ff,transparent)]">
+          {children}
+        </div>
+      </body>
     </html>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,43 @@
-export default function Home() {
-  return <h1>Hello World</h1>
+import MaxWidthWrapper from '@/components/MaxWidthWrapper'
+import { Button, buttonVariants } from '@/components/ui/button'
+import { ArrowRightIcon } from '@radix-ui/react-icons'
+import Link from 'next/link'
+
+const HomePage = () => {
+  return (
+    <MaxWidthWrapper className="mb-12 mt-28 flex flex-col items-center justify-center text-center sm:mt-40">
+      <HeroSection />
+    </MaxWidthWrapper>
+  )
 }
+
+const HeroSection = () => {
+  return (
+    <div className="flex flex-col items-center space-y-5">
+      <div>
+        <p className="mx-auto mb-4 flex max-w-fit items-center justify-center space-x-2 rounded-full bg-white bg-gradient-to-r from-blue-600 to-purple-300 px-7 py-2 font-semibold text-white shadow-md backdrop-blur transition-all md:text-lg lg:text-lg">
+          Klipp is now public!
+        </p>
+      </div>
+      <div>
+        <h1 className="max-w-4xl text-5xl font-bold md:text-6xl lg:text-7xl">
+          Chat with your <span className="text-blue-600">documents</span> in
+          seconds.
+        </h1>
+      </div>
+      <div>
+        <Link href="/dashboard">
+          <Button
+            className="transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110"
+            size="lg"
+          >
+            Get Started
+            <ArrowRightIcon className="ml-2 h-5 w-5" />
+          </Button>
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+export default HomePage
