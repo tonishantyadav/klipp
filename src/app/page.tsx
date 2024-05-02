5d6dc858ac28674edbf48be35b5abaa20e0360b7
Nishant Yadav <inishantyadav24@gmail.com>
2024-05-02 21:46:24 +0530

    Refactor homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import Container from '@/components/Container'
+import ScrollToTopButton from '@/components/ScrollToTopButton'
 import { Button } from '@/components/ui/button'
 import { ArrowRightIcon } from '@radix-ui/react-icons'
 import Image from 'next/image'
 import Link from 'next/link'
 import dashboardPreview from '../../public/dashboard-preview.jpg'
-import ScrollToTopButton from '@/components/ScrollToTopButton'
+import hightVoltage from '../../public/high-voltage.svg'
 
 const HomePage = () => {
   return (
@@ -16,7 +17,7 @@ const HomePage = () => {
         <div className="flex flex-col items-center justify-center">
           <MainSection />
         </div>
-        <div className="flex items-center justify-center">
+        <div className="flex flex-col items-center justify-center space-y-20 pb-10">
           <FooterSection />
         </div>
       </Container>
@@ -28,13 +29,24 @@ const HomePage = () => {
 const HeaderSection = () => {
   return (
     <>
-      <p className="my-5 rounded-full bg-gradient-to-r from-blue-600 to-purple-300 px-7 py-2 font-semibold text-white shadow-md transition-all md:text-lg lg:text-lg">
-        Klipp is now public!
+      <p className="my-5 flex items-center rounded-full bg-gradient-to-r from-blue-600 to-purple-300 px-7 py-2 font-semibold text-white shadow-md transition-all md:text-lg lg:text-lg">
+        Klipp is now public{' '}
+        <span className="px-1">
+          <Image
+            src={hightVoltage}
+            alt="High voltage icon"
+            className="h-[20px] w-[20px]"
+          />
+        </span>
       </p>
       <h1 className="max-w-4xl text-5xl font-bold text-slate-700/90 md:text-6xl lg:text-7xl">
         Chat with your <span className="text-blue-600">documents</span> in
         seconds.
       </h1>
+      <p className="max-w-prose text-zinc-500 sm:text-lg">
+        Klipp allows you to have conversations with any PDF document. Simply
+        upload your file and start asking questions right away.
+      </p>
       <div>
         <Link href="/dashboard">
           <Button
@@ -56,7 +68,7 @@ const MainSection = () => {
       <Image
         className="rounded-lg shadow-2xl ring-1 ring-gray-900/10"
         src={dashboardPreview}
-        alt="Dashboard preiview showcase"
+        alt="Dashboard preview"
         width={1364}
         height={866}
         quality={100}
@@ -69,53 +81,68 @@ const MainSection = () => {
 const FooterSection = () => {
   return (
     <>
-      <ol className="space-y-4 px-2 md:flex md:space-x-12 md:space-y-0">
-        <li className="md:flex-1">
-          <div className="flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4 md:border-l-0 md:border-t-2 md:pb-0 md:pl-0 md:pt-4">
-            <span className="text-sm font-medium text-blue-600">Step 1</span>
-            <span className="text-xl font-semibold text-slate-700">
-              Sign up for an account
-            </span>
-            <p className="mt-2 text-zinc-700">
-              Either starting out with a free plan or choose our{' '}
-              <span>
-                <Link
-                  href="/pricing"
-                  className="text-blue-600 hover:underline hover:underline-offset-2"
-                >
-                  pro
-                </Link>
-              </span>{' '}
-              plan.
-            </p>
-          </div>
-        </li>
-        <li className="md:flex-1">
-          <div className="flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4 md:border-l-0 md:border-t-2 md:pb-0 md:pl-0 md:pt-4">
-            <span className="text-sm font-medium text-blue-600">Step 2</span>
-            <span className="text-xl font-semibold text-slate-700">
-              Upload your PDF file
-            </span>
-            <span className="mt-2 text-zinc-700">
-              We&apos;ll process your file and make it ready for you to chat
-              with.
-            </span>
-          </div>
-        </li>
-        <li className="md:flex-1">
-          <div className="flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4 md:border-l-0 md:border-t-2 md:pb-0 md:pl-0 md:pt-4">
-            <span className="text-sm font-medium text-blue-600">Step 3</span>
-            <span className="text-xl font-semibold text-slate-700">
-              Start asking questions
-            </span>
-            <span className="mt-2 text-zinc-700">
-              It&apos;s that simple. Try out Klipp today - it really takes less
-              than a minute.
-            </span>
-          </div>
-        </li>
-      </ol>
+      <div className="container mx-auto flex flex-col items-center  justify-center space-y-5 text-center">
+        <h2 className="max-w-fit text-5xl font-semibold text-slate-700/90 md:text-6xl lg:text-7xl">
+          Start chatting in <span className="text-blue-600">minutes.</span>
+        </h2>
+        <p className="max-w-fit  text-zinc-500">
+          Chatting to your PDF files has never been easier than with Klipp.
+        </p>
+      </div>
+      <div>
+        <StepsSection />
+      </div>
     </>
+  )
+}
+
+const StepsSection = () => {
+  return (
+    <ol className="space-y-4 p-2 md:flex md:space-x-12 md:space-y-0">
+      <li className="md:flex-1">
+        <div className="flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4 md:border-l-0 md:border-t-2 md:pb-0 md:pl-0 md:pt-4">
+          <span className="text-sm font-medium text-blue-600">Step 1</span>
+          <span className="text-xl font-semibold text-slate-700">
+            Sign up for an account
+          </span>
+          <p className="mt-2 text-zinc-700">
+            Either starting out with a free plan or choose our{' '}
+            <span>
+              <Link
+                href="/pricing"
+                className="text-blue-600 hover:underline hover:underline-offset-2"
+              >
+                pro
+              </Link>
+            </span>{' '}
+            plan.
+          </p>
+        </div>
+      </li>
+      <li className="md:flex-1">
+        <div className="flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4 md:border-l-0 md:border-t-2 md:pb-0 md:pl-0 md:pt-4">
+          <span className="text-sm font-medium text-blue-600">Step 2</span>
+          <span className="text-xl font-semibold text-slate-700">
+            Upload your PDF file
+          </span>
+          <span className="mt-2 text-zinc-700">
+            We&apos;ll process your file and make it ready for you to chat with.
+          </span>
+        </div>
+      </li>
+      <li className="md:flex-1">
+        <div className="flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4 md:border-l-0 md:border-t-2 md:pb-0 md:pl-0 md:pt-4">
+          <span className="text-sm font-medium text-blue-600">Step 3</span>
+          <span className="text-xl font-semibold text-slate-700">
+            Start asking questions
+          </span>
+          <span className="mt-2 text-zinc-700">
+            It&apos;s that simple. Try out Klipp today - it really takes less
+            than a minute.
+          </span>
+        </div>
+      </li>
+    </ol>
   )
 }
 
