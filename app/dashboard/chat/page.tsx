d9f6a0366218c11fbde91daa728204b88e695b73
Nishant Yadav <inishantyadav24@gmail.com>
2024-05-14 11:32:48 +0530

    Build layout of a chat-page

---
+++ app/dashboard/chat/page.tsx
@@ -0,0 +1,16 @@
+const ChatPage = ({
+  searchParams,
+}: {
+  searchParams?: { [key: string]: string | string[] | undefined }
+}) => {
+  console.log(searchParams?.file)
+
+  return (
+    <div className="grid h-screen grid-cols-1 md:grid-cols-[60%_40%] lg:grid-cols-[60%_40%]">
+      <div className="border border-red-500">File Container</div>
+      <div className="border border-blue-500">Chat Container</div>
+    </div>
+  )
+}
+
+export default ChatPage
