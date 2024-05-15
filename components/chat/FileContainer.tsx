5c079beb040d1a45b3a8434a10d652d05bb29444
Nishant Yadav <inishantyadav24@gmail.com>
2024-05-15 16:20:24 +0530

    Add navigation

--- components/chat/FileContainer.tsx
+++ components/chat/FileContainer.tsx
@@ -1,10 +1,16 @@
 import { PdfRenderer } from '@/components/chat/PdfRenderer'
 import { File } from '@prisma/client'
+import { FileTextIcon } from '@radix-ui/react-icons'
 
 export const FileContainer = ({ file }: { file: File }) => {
   return (
     <div className="flex h-full flex-col gap-2 overflow-hidden">
-      <div className="border p-2">Topbar</div>
+      <div className="flex items-center gap-1.5 border-b border-gray-300 p-2">
+        <FileTextIcon className="text-blue-600" />
+        <span className="text-sm text-zinc-700">
+          {file.name.length > 64 ? `${file.name.slice(0, 64)}...` : file.name}
+        </span>
+      </div>
       <div className="h-full overflow-hidden">
         <PdfRenderer fileUrl={file.url} />
       </div>
--- components/chat/PdfRenderer.tsx
+++ components/chat/PdfRenderer.tsx
@@ -1,17 +1,20 @@
 'use client'
 
-import { Document, Page, pdfjs } from 'react-pdf'
-import { useResizeDetector } from 'react-resize-detector'
 import { Loader } from '@/components/ui/loader'
+import { ArrowLeftIcon, ArrowRightIcon } from '@radix-ui/react-icons'
+import { useState } from 'react'
+import { Document, Page, pdfjs } from 'react-pdf'
 import 'react-pdf/dist/esm/Page/AnnotationLayer.css'
 import 'react-pdf/dist/esm/Page/TextLayer.css'
-import { useState } from 'react'
+import { useResizeDetector } from 'react-resize-detector'
+import { Button } from '@/components/ui/button'
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`
 
 export const PdfRenderer = ({ fileUrl }: { fileUrl: string }) => {
   const { ref, width, height } = useResizeDetector()
   const [numPages, setNumPages] = useState(0)
+  const [currentPage, setCurrentPage] = useState(1)
   const [isLoading, setIsLoading] = useState(true)
 
   const onLoadSuccess = ({ numPages }: { numPages: number }) => {
@@ -19,28 +22,64 @@ export const PdfRenderer = ({ fileUrl }: { fileUrl: string }) => {
     setNumPages(numPages)
   }
 
+  const goToPrevPage = () => {
+    setCurrentPage((prevPage) => {
+      const newPage = Math.max(prevPage - 1, 1)
+      return newPage
+    })
+  }
+
+  const goToNextPage = () => {
+    setCurrentPage((prevPage) => {
+      const newPage = Math.min(prevPage + 1, numPages)
+      return newPage
+    })
+  }
+
   return (
-    <div ref={ref} className="flex h-full overflow-y-auto overflow-x-hidden">
+    <>
       {isLoading && (
         <div className="flex h-full w-full flex-col items-center justify-center">
           <Loader />
         </div>
       )}
-      <Document file={fileUrl} onLoadSuccess={onLoadSuccess} loading={null}>
-        <div>
-          {!isLoading &&
-            numPages &&
-            Array.from(new Array(numPages), (el, index) => (
-              <Page
-                key={`page_${index + 1}`}
-                pageNumber={index + 1}
-                width={width}
-                height={height}
-                loading={null}
-              />
-            ))}
-        </div>
-      </Document>
-    </div>
+      <div
+        ref={ref}
+        className="hide-scrollbar flex h-full flex-col overflow-auto"
+      >
+        {!isLoading && (
+          <div className="flex items-center justify-between p-2">
+            <Button
+              className="rounded-full"
+              variant="outline"
+              size="icon"
+              disabled={currentPage <= 1}
+              onClick={goToPrevPage}
+            >
+              <ArrowLeftIcon />
+            </Button>
+            <span className="text-sm text-zinc-700">{`Page ${currentPage} of ${numPages}`}</span>
+            <Button
+              className="rounded-full"
+              variant="outline"
+              size="icon"
+              disabled={currentPage >= numPages}
+              onClick={goToNextPage}
+            >
+              <ArrowRightIcon />
+            </Button>
+          </div>
+        )}
+        <Document file={fileUrl} onLoadSuccess={onLoadSuccess} loading={null}>
+          <Page
+            key={`page_${currentPage}`}
+            pageNumber={currentPage}
+            width={width}
+            height={height}
+            loading={null}
+          />
+        </Document>
+      </div>
+    </>
   )
 }
