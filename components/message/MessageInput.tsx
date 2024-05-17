aee0c2276da414afc51ae5bcb5e8c93b511b0ee2
Nishant Yadav <inishantyadav24@gmail.com>
2024-05-17 12:58:29 +0530

    Disable message-input button while loading the file

--- components/message/MessageInput.tsx
+++ components/message/MessageInput.tsx
@@ -1,10 +1,15 @@
+'use client'
+
 import React from 'react'
 import { Textarea } from '@/components/ui/textarea'
+import { usePdfStore } from '@/store/PdfStore'
 
 export const MessageInput = () => {
+  const { isLoading } = usePdfStore()
+
   return (
     <div>
-      <Textarea placeholder="What's in your mind?" />
+      <Textarea placeholder="What's in your mind?" disabled={isLoading} />
     </div>
   )
 }
